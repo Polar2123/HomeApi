01cd617eda912ad5b278bd3665ded41d76da6172
sivert1 <sivertbronebakk@gmail.com>
2026-06-23 16:26:00 +0200

    Add Disconnect Detection and Connection Waiting

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 import socket
+import queue
+import threading
+
 def main():
     sock = socket.socket()
+    connected = False
     with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock:
-        sock.connect(("localhost",9988))
-    
-        sock.send(b"SUB topic")
+        while not connected:
+            try:
+                connected = startConnection(sock)
+            except:
+                pass
 
-        byteResult = sock.recv(1024)
-        result = byteResult.decode('utf-8')
+        handleConnection(sock)
+        
 
-        print(result)
+def handleConnection(socket):
+    try:
+        while True:
+            data = socket.recv(1024)
+            if not data: raise Exception("The Socket has been Closed!")
+             
 
+    except Exception as e:
+        print(repr(e))
+        print("Closing down the API!")
+
+def startConnection(socket) -> bool:
+    socket.connect(("localhost",9988))
+    socket.send(b"CONN api")
+    byteResult = socket.recv(1024)
+    result = decode(byteResult)
+    return result == "CONNACK"
+
+def decode(byteResult):
+    return byteResult.decode('utf-8')
 
 if __name__ == "__main__":
     main()
